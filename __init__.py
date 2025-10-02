ae91b8d133800c7c15c495ddf7c711908578524f
Jeremy <jeremyrch2011@hotmail.com>
2025-10-02 13:47:27 -0500

    cambios

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 # ml_csv_lib/__init__.py
-from .ml_csv_lib.data_loader import DataLoader
-from .ml_csv_lib.encoders import Encoders
-from .ml_csv_lib.feature_engineer import FeatureEngineer
-from .ml_csv_lib.data_selector import DataSelector
-from .ml_csv_lib.strategies import SupervisedStrategy, UnsupervisedStrategy, RegressionStrategy
-from .ml_csv_lib.treatments import SupervisedTreatmentType, UnsupervisedTreatmentType, RegressionTreatmentType, apply_supervised_treatment, apply_unsupervised_treatment, apply_regression_treatment
+from  .data_loader import DataLoader
+from .encoders import Encoders
+from .feature_engineer import FeatureEngineer
+from .data_selector import DataSelector
+from .strategies import SupervisedStrategy, UnsupervisedStrategy, RegressionStrategy
+from .treatments import SupervisedTreatmentType, UnsupervisedTreatmentType, RegressionTreatmentType, apply_supervised_treatment, apply_unsupervised_treatment, apply_regression_treatment
